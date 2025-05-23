4546efa9e52d79b05851326c30df504829cf4eda
itc-s24017 <s24017@std.it-college.ac.jp>
2025-05-23 10:44:38 +0900

    課題

---
+++ D49Q18kadai.java
@@ -0,0 +1,9 @@
+public class D49Q18kadai {
+    public static void main(String[] args) {
+        int total = 750;
+        int capacity = 300;
+        int cargo = 20;
+
+        System.out.println((double)(total / capacity * cargo));
+    }
+}
