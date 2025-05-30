8be6a769c467ee88c91bd11f0081fd940a87374c
itc-s24017 <s24017@std.it-college.ac.jp>
2025-05-30 12:36:38 +0900

    課題

---
+++ D5027kadai.java
@@ -0,0 +1,22 @@
+public class D5027kadai {
+    public static void main(String[] args) {
+        int control = 3;
+        String mode;
+
+        switch (control) {
+            case 1:
+                mode = "Warm";
+                break;
+            case 2:
+                mode = "Cool";
+                break;
+            case 3:
+                mode = "Wind";
+                break;
+            default:
+                mode = "";
+                break;
+        }
+        System.out.println(mode);
+    }
+}
