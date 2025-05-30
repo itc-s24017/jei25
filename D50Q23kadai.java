53d18461fa96b81d641783dd51ec5d0db8a6790c
itc-s24017 <s24017@std.it-college.ac.jp>
2025-05-30 11:56:38 +0900

    課題

---
+++ D50Q23kadai.java
@@ -0,0 +1,11 @@
+public class D50Q23kadai {
+    String code;
+    public static void main(String[] args) {
+        D50Q23kadai obj = new D50Q23kadai();
+        obj.setCode("Code");
+    }
+    void setCode(String c) {
+        code = c;
+        System.out.println(code);
+    }
+}
