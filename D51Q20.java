d23ba5b662f8943af8c1764520da51e6f9600405
itc-s24017 <s24017@std.it-college.ac.jp>
2025-06-27 13:05:58 +0900

    課題

---
+++ D51Q20.java
@@ -0,0 +1,8 @@
+public class D51Q20 {
+    public static void main(String[] args) {
+        boolean flg = true;
+        int n = 6;
+        char c = (flg = n > 8) ? 'Y' : 'N';
+        System.out.println(c);
+    }
+}
