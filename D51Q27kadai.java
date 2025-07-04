d9ae6ef4c268a4edb201ccdebd94b35b3b63aaaa
itc-s24017 <s24017@std.it-college.ac.jp>
2025-07-04 13:07:39 +0900

    課題

---
+++ D51Q27kadai.java
@@ -0,0 +1,12 @@
+enum TrafficLight {
+    RED,
+    YELLOW,
+    GREEN
+}
+
+public class D51Q27kadai {
+    public static void main(String[] args) {
+        TrafficLight signal = TrafficLight.GREEN;
+        System.out.println("青");
+    }
+}
