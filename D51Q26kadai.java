db6d75cf7b7423367672fec0a0a07f92a85fee79
itc-s24017 <s24017@std.it-college.ac.jp>
2025-07-04 12:59:58 +0900

    課題

--- D51Q26kadai.java
+++ D51Q26kadai.java
@@ -1,20 +1,24 @@
 public class D51Q26kadai {
     public static void main(String[] args) {
         int number = 1;
+        String result = "";
 
-        // switch文を使用して値に応じた処理を行う
+        // switch文：numberの値に応じて処理を分岐
         switch (number) {
-            case 1: // case文：numberが1のときにこのブロックが実行される
-                System.out.println("one");
-                break; // break文：ここでswitch文の処理を終了。次のcaseへは進まない
+            case 1:
+                // case文：numberが1と一致した場合、このブロックが実行される
+                result = "one";
+                break; // break文：この行でswitch文から抜けて、他のcaseは実行されない
+
             case 2:
-                System.out.println("two");
+                result = "two";
                 break;
-            case 3:
-                System.out.println("three");
-                break;
-            default: // default文：どのcaseにも一致しない場合に実行される
-                System.out.println("not one, two, or three");
+
+            default:
+                result = "other";
+                // default：どのcaseにも一致しなかった場合に実行される（breakがなくても最後なので抜ける）
         }
+
+        System.out.println(result); // 出力: "one"
     }
 }
