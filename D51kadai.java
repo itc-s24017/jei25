853377dfcaa739a289836233c9d3cd9c979a489d
itc-s24017 <s24017@std.it-college.ac.jp>
2025-07-11 12:04:54 +0900

    課題

--- D51kadai.java
+++ D51kadai.java
@@ -1,9 +1,7 @@
 public class D51kadai {
     public static void main(String[] args) {
-        // コマンドライン引数の2番目（args[1]）をdouble型に変換
         double value = Double.parseDouble(args[1]);
 
-        // 切り上げて表示
         System.out.println(Math.ceil(value));
     }
 }
